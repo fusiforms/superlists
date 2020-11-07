c6e15af6cfe9cdf0663d34bba7c5e0aaba79b1e6
Anthony M <antmontalt@me.com>
2020-11-07 21:34:16 +1100

    Add basic html to home page

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 """
 Views for the lists app of superlists
 """
+from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_page():
+def home_page(request):
     """
     View to render our home page
     """
-    pass
+    return HttpResponse('<html><title>To-Do Lists</title></html>')
