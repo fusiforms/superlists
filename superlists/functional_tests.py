e4d6e59a018e2707c1f64ff81171b110d083c52b
Anthony M <antmontalt@me.com>
2020-11-07 20:54:35 +1100

    Use unnittest in functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,17 +1,60 @@
 """
-Initial functional test for TDD
+Functional test for superlists app (a To Do List)
 """
+import unittest
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-# browser.maximize_window()
-# browser.get('https://www.google.com/')
-# browser.find_element_by_name('q').send_keys('morning')
-# browser.find_element_by_name('q').send_keys(Keys.ENTER)
-# browser.close()
-# print("Sample test case run, and browser closed.")
+class NewVisitorTest(unittest.TestCase):
+    """
+    Class of TestCase to hold the function tests
+    for a new visitor to the superlists site
+    """
 
-assert 'Django' in browser.title
+    def setUp(self):
+        """
+        Initiates the new visitor tests by attaching to the browser
+        """
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        """
+        Finalise the new visitor tests by quitting the browser
+        """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        New visitor test that starts a new list, adds some items and
+        verifies that the list can be found at itr URL
+        """
+
+        # Edith has heard about a new online to-do app.
+        # She goes to the browser to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish writing the tests!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box
+
+        # When she hits enter, the page updates and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list.
+        # She see some explanatory text noting that the sire has generated a unique URL for her.
+
+        # She visits the URL and sees her to-do list is still there.
+
+        # Satisfied, she goes to sleep.
+
+if __name__ == '__main__':
+    unittest.main()
