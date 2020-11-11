05c1f731a38006ff3c06bcb203f358916601fcb6
Anthony M <antmontalt@me.com>
2020-11-11 20:59:32 +1100

    Use Django test client for HomePageTest

--- lists/tests.py
+++ lists/tests.py
@@ -1,32 +1,16 @@
 """
 Unit tests for the lists app
 """
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     """
-    Test for the list app's home page
+    Tests for the list app's home page
     """
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_uses_home_template(self):
         """
-        Unit test to check that the root URL can be resolved and
-        calls the home_page view
+        Unit test to check that home page uses the correct home page template
         """
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """
-        Unit test to chcek that home page returns html tags and a known title
-        """
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
