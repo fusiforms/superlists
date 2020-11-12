2fe174202dde71d97ed7f0adbad943b66ee9f13a
Anthony M <antmontalt@me.com>
2020-11-12 21:32:09 +1100

    Refactor FT with check_for_row_in_list_table()

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -25,6 +25,15 @@ class NewVisitorTest(unittest.TestCase):
         """
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """
+        Helper function to check if supplied row text is in an HTML table with
+        id 'id_list_table'
+        """
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         New visitor test that starts a new list, adds some items and
@@ -47,18 +56,14 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # She types "Buy peacock feathers" into a text box
+        # She types "Buy peacock feathers" into a text box and hits Enter
         inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',
-                      [row.text for row in rows])
+        # When she hits enter, the page updates and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -68,10 +73,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list.
         # She see some explanatory text noting that the sire has generated a unique URL for her.
