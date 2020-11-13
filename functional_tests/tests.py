1a24a31d266446f31ffc0261d19cafbce66985fd
Anthony M <antmontalt@me.com>
2020-11-13 21:49:29 +1100

    Remove implicit waits from FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,10 @@ Functional test for superlists app (a To Do List)
 import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -25,14 +28,22 @@ class NewVisitorTest(LiveServerTestCase):
         """
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
+    def wait_for_row_in_list_table(self, row_text):
         """
         Helper function to check if supplied row text is in an HTML table with
-        id 'id_list_table'
+        id 'id_list_table' and retrying up to a maxiumm wait time.
         """
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
@@ -59,22 +70,20 @@ class NewVisitorTest(LiveServerTestCase):
         # She types "Buy peacock feathers" into a text box and hits Enter
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # When she hits enter, the page updates and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list.
         # She see some explanatory text noting that the sire has generated a unique URL for her.
